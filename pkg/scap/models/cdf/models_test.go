1f58df6c79f3b0fe728af02dea4fa25c8e481889
Šimon Lukašík <isimluk@fedoraproject.org>
2020-05-25 16:30:22 +0200

    Improve our first and the only test
    tiny little bit

--- pkg/scap/models/cdf/models_test.go
+++ pkg/scap/models/cdf/models_test.go
@@ -10,6 +10,8 @@ import (
 func TestRFCFeedParsing(t *testing.T) {
 	doc, err := scap_document.ReadDocumentFromFile("../../../../examples/scap/xccdf/1.2/test_xccdf_complex_check_nand.xccdf.xml")
 	assert.Nil(t, err)
-	assert.NotNil(t, doc.Benchmark)
-	assert.Equal(t, doc.Benchmark.XMLName.Space, "http://checklists.nist.gov/xccdf/1.2")
+	bench := doc.Benchmark
+	assert.NotNil(t, bench)
+	assert.Equal(t, bench.XMLName.Space, "http://checklists.nist.gov/xccdf/1.2")
+	assert.Equal(t, bench.Id, "testing-xcccdf")
 }
