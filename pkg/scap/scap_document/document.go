70701b51b4e50faea9700cff91633056f0ec88e5
Šimon Lukašík <isimluk@fedoraproject.org>
2020-05-25 16:11:17 +0200

    Initial project structure
    Created by running:
    ./../xsd2go/gocomply_xsd2go convert ../../openscap/openscap/schemas/xccdf/1.2/xccdf_1.2.xsd github.com/gocomply/scap pkg/scap/models
    
    Autogeneration is still pretty much work in progres.

---
+++ pkg/scap/scap_document/document.go
@@ -0,0 +1,49 @@
+package scap_document
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/gocomply/scap/pkg/scap/models/cdf"
+)
+
+const (
+	xccdfBenchmarkElement = "Benchmark"
+)
+
+type Document struct {
+	XMLName xml.Name `json:"-"`
+	*cdf.Benchmark
+}
+
+func ReadDocument(r io.Reader) (*Document, error) {
+	d := xml.NewDecoder(r)
+	for {
+		token, err := d.Token()
+		if err != nil || token == nil {
+			return nil, fmt.Errorf("Could not decode XML: %v", err)
+		}
+		switch startElement := token.(type) {
+		case xml.StartElement:
+			switch startElement.Name.Local {
+			case xccdfBenchmarkElement:
+				var bench cdf.Benchmark
+				if err := d.DecodeElement(&bench, &startElement); err != nil {
+					return nil, err
+				}
+				return &Document{Benchmark: &bench}, nil
+			}
+		}
+	}
+	return nil, fmt.Errorf("Could not parse input file")
+}
+
+func ReadDocumentFromFile(filepath string) (*Document, error) {
+	reader, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	return ReadDocument(reader)
+}
