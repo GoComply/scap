c7e1682471905235a4819a585732b92591f65d9a
Šimon Lukašík <isimluk@fedoraproject.org>
2020-06-08 16:45:27 +0200

    Improve sanity test for cpe_dict parsing

--- pkg/scap/models/cpe_dict/models_test.go
+++ pkg/scap/models/cpe_dict/models_test.go
@@ -17,6 +17,13 @@ func TestSimpleParsing(t *testing.T) {
 	assert.Equal(t, cpeList.XMLName.Space, "http://cpe.mitre.org/dictionary/2.0")
 	items := cpeList.CpeItem
 	assert.Equal(t, len(items), 7)
+	item0 := items[0]
+	assert.Equal(t, item0.Name, "cpe:/a:1024cms:1024_cms:0.7")
+	assert.Equal(t, len(item0.Title), 1)
+	assert.Equal(t, item0.Title[0].Text, "1024cms.org 1024 CMS 0.7")
+
 	item2 := items[2]
 	assert.Equal(t, len(item2.References.Reference), 2)
+	assert.Equal(t, item2.References.Reference[0].Href, "http://wordpress.org/plugins/xhanch-my-twitter/changelog/")
+	assert.Equal(t, item2.References.Reference[0].Text, "Xhance My Twitter Plugin page on Wordpress")
 }
