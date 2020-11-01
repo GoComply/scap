cba2be282116c794bf0cb3521e6920c8b1620f36
Guy Rozendorn <guy@rzn.co.il>
2020-11-01 14:30:48 +0200

    fixing DefinitionClasses()

--- pkg/scap/models/oval_def/oval_definitions.go
+++ pkg/scap/models/oval_def/oval_definitions.go
@@ -1,16 +1,18 @@
 package oval_def
 
+import "github.com/gocomply/scap/pkg/scap/models/oval"
+
 func (defs *OvalDefinitions) ScapVersion() string {
 	return defs.Generator.ScapVersion()
 }
 
-func (defs *OvalDefinitions) DefinitionClasses() []DefinitionClass {
-	res := []DefinitionClass{}
-	seen := map[string]struct{}{}
+func (defs *OvalDefinitions) DefinitionClasses() []oval.ClassEnumeration {
+	res := []oval.ClassEnumeration{}
+	seen := map[oval.ClassEnumeration]struct{}{}
 
 	for _, def := range defs.Definitions.Definition {
 		if _, found := seen[def.Class]; !found {
-			res = append(res, DefinitionClass(def.Class))
+			res = append(res, def.Class)
 			seen[def.Class] = struct{}{}
 		}
 	}
